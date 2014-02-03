c26458cd3733e66161a243e4d5a970ad2cfd84fa
Frydac <emile.vrijdags+github@gmail.com>
2014-02-03 18:46:26 +0100

    Cleaned some code, added buttons
    Buttons are in test mode, they ad a row but no content yet

--- 252nd.js
+++ 252nd.js
@@ -1,4 +1,9 @@
-﻿//----- TODO:
+﻿//This is my first Javascript project ever, so I'm learning on the go
+//I put a lot of comments everywhere, mainly so hopefully I can understand it later myself
+
+
+
+//----- TODO:
 //-- organize code better, use .then .when functions to deal with async ajax:
 //http://jqfundamentals.com/chapter/ajax-deferreds
 //http://stackoverflow.com/questions/14465177/multiple-ajax-calls-wait-for-last-one-to-load-then-execute#
@@ -6,6 +11,7 @@
 //-- make input for minimum played time/month
 //-- make input for nr of months
 //-- make search while you type for outfit name
+//--
 
 //----- usefull links:
 //soe rest api information (incomplete and faulty information there :s )
@@ -17,6 +23,13 @@
 //for testing purposes
 //frydac char id: "5428010618030935617"
 
+//debug variable, set higher than 0 when needed
+//this will cause the app to only query max_members_debug amount of members
+//and not every member of the outfit.
+var max_members_debug = -1;
+
+//global variable of type Outfit, will be initialized later
+var outfit = null;
 
 //class/struct declaration for outfit information
 function Outfit() {
@@ -31,8 +44,20 @@ function Outfit() {
     //array of Outfit_members where the SOE API doesn't provide detailed information
     this.members_broken_info = [];
 
-    //TODO: leader
+    //dont know if its ok that this is in the function, in eloquent js its outside, should move to different file and move outside maybe
+    Outfit.prototype.find_member = function (name) {
+        var result = this.members.filter(function (member) {
+            return member.name === name;
+        });
+        //console.log(result);
+        //I know for sure that the name is unique
+        return result[0];
+    };
+
+    //TODO: 
     //this.leader = new Outfit_member;
+    //this.prototype.sort_members
+    //this.prototype.find_member
 }
 
 
@@ -58,17 +83,21 @@ var member_playtimeinfo_done_counter = 0;
 
 //entry point: start doing things when document is ready
 $(document).ready(function () {
-
-    //inform user we are waiting for response:
-    $("#outfit").html("Waiting for outfit information..");
-
-    //get outfit information including memberlist, put outfit in one object, members in another
-
-    var outfit_name = "252nd Spec Ops";
+     var outfit_name = "252nd Spec Ops";
     //if you want to check other outfits edit this variable, for example uncomment following line
     //outfit_name = "INI Elite";
 
-    var outfit;
+    //this needs a better name :D
+    initialize_document(outfit_name);
+
+});
+
+
+function initialize_document(outfit_name) {
+    //get outfit information including memberlist, put outfit in one object, members in another
+    //inform user we are waiting for response:
+    $("#outfit").html("Waiting for outfit information..");
+
     var members_stat_history_REST_responses;
     //this is an async function, so it will return before the ajax response (jqXHR) is ready
     var outfitinfo_memberlist_REST_response = get_api_info_outfit(outfit_name);
@@ -92,20 +121,23 @@ $(document).ready(function () {
         //again the jquery .next function. Here you see that the abstraction is better protected, else I would have
         //needed to jump out of this function and broken the flow of this function's abstraction.
         $.when.apply(this, members_stat_history_REST_responses).done(function () {
-            console.log(arguments);
-            console.log(outfit.members);
             //when we get here it means that all ajax requests in members_stat_history_REST_responses are done
             //extract_members_stat_history(arguments);
             //need to rewrite this
             outfit.members.sort(function (m1, m2) {
                 return parseInt(m1.playtime_per_month[1]) - parseInt(m2.playtime_per_month[1]);
             });
-            create_members_HTML(outfit.members);
+            var members_HTML = create_members_HTML(outfit.members);
+            $("#members").html(members_HTML);
+
+            $(".show_extra").bind("click", show_member_extra_info);
             create_broken_members_HTML(outfit.members_broken_info);
 
         });
     });
-});
+
+
+}
 
 // makes the outfit ajax call and returns the jQuery XML HTTP Request object
 function get_api_info_outfit(outfit_name) {
@@ -138,18 +170,76 @@ function extract_outfit_information(ajaxResponse) {
 function get_api_info_members_stat_history(members) {
     var responses = [];
 
-    for (member_index in members) {
+    var step_size = 10;
 
-        //i must make a function call here to have a copy of the reference to members, because the success function comes later
-        var response = get_api_info_member_stat_history(members[member_index]);
+    //initialize counter
+    update_get_stat_history_counter(step_size, members);
+
+
+
+    for (member_index = 0; member_index < members.length; member_index += step_size) {
+        var response = get_api_info_n_members_stat_history(members, member_index, step_size);
         responses.push(response);
-
-
     }
+
+
 
     return responses;
 }
 
+function get_api_info_n_members_stat_history(members, index, n) {
+    var n_member_char_ids = "";
+    for (var i = index; i < index + n && i < members.length; i++) {
+        n_member_char_ids += members[i].character_id + ",";
+    }
+
+    var stat_history_for_char_REST_URL = "http://census.soe.com/s:252nd/get/ps2:v2/character/?character_id="
+    + n_member_char_ids
+    + "&c:resolve=stat_history";
+
+    //console.log(stat_history_for_char_REST_URL);
+
+    var response = $.ajax({
+        url: stat_history_for_char_REST_URL,
+        dataType: "jsonp",
+        success: function (response, text_status, jqXHR) {
+            //console.log(response);
+
+            //we asked for n chars, so loop through the response character list
+            for (char_list_index in response.character_list) {
+                var current_members_index = index + parseInt(char_list_index);
+                var current_member = members[current_members_index];
+                if (current_member.character_id !== response.character_list[char_list_index].character_id) {
+                    console.log("characters returned arent in the same order as in the query! (or my code is bugged ;) )")
+                }
+                var member_stat_history = response.character_list[char_list_index].stats.stat_history;
+                //get the right stat history array index
+                for (mem_stat_index in member_stat_history) {
+                    if (member_stat_history[mem_stat_index].stat_name === "time") {
+                        //add all the months, its a list of fields/properties, not an array
+                        for (month_field in member_stat_history[mem_stat_index].month) {
+                            if (member_stat_history[mem_stat_index].month.hasOwnProperty(month_field)) {
+                                current_member.playtime_per_month.push(member_stat_history[mem_stat_index].month[month_field]);
+                            }
+                        };
+                    }
+                }
+
+            }
+
+            //update counter in webpage to inform user
+            update_get_stat_history_counter(n, members)
+
+
+
+            //console.log(members);
+        }
+    });
+
+    return response;
+
+
+}
 //
 function get_api_info_member_stat_history(member) {
     var member_char_id = member.character_id;
@@ -189,7 +279,16 @@ function extract_member_list_information(ajaxResponse, members, members_broken_i
     //character_id, name, battle_rank, rank_ordinal + rank, member_since_date, times.creation_date, times.last_login_date, times.last_save_date
 
     //loop through all the members in the response and extract appropriate information
-    for (member_index in ajaxResponse.outfit_list[0].members) {
+    //for (member_index in ajaxResponse.outfit_list[0].members) {
+    //DEBUG:
+
+    var max_members;
+    if (max_members_debug <= 0)
+        max_members = ajaxResponse.outfit_list[0].members.length;
+    else
+        max_members = max_members_debug;
+
+    for (member_index = 0; member_index < max_members; member_index++) {
 
         //use this var to make the dereferencing little less verbose
         var response_member = ajaxResponse.outfit_list[0].members[member_index];
@@ -238,7 +337,6 @@ function create_outfit_HTML(outfit) {
     return outfit_HTML;
 }
 
-
 //creates a table with the members info and outputs it to the webpage
 function create_members_HTML(members) {
 
@@ -250,6 +348,7 @@ function create_members_HTML(members) {
     var membersHTML = "<h2>Members</h2>";
     //start table with class alternate_color
     membersHTML += "<table class=\"alternate_color\" >";
+    //  membersHTML += "<thead>";
     //create a table row (tr) with table headers (th)
     membersHTML += "<tr>"
         + "<th>#</th>"
@@ -258,7 +357,7 @@ function create_members_HTML(members) {
         + "<th>BR</th>"
         + "<th>Member since</th>";
     //get the number of months from one member and make the tableheaders with appropriate month names
-    console.log(members[0].playtime_per_month);
+    //console.log(members[0].playtime_per_month);
     for (month_index in members[0].playtime_per_month) {
         membersHTML += "<th>"
             + get_month_name((d.getMonth() + 12 - month_index) % 12)
@@ -266,18 +365,23 @@ function create_members_HTML(members) {
     }
 
     membersHTML += "</tr>";
-
+    //  membersHTML += "</thead>";
+    //  membersHTML += "<tbody>";
     //loop through members with good info and create html table rows (tr) and table data (td)
     var member;
     var member_counter = 1;
     for (member_index in members) {
         member = members[member_index];
-        membersHTML += "<tr>"
-            + "<td>" + member_counter + "</td>"
-            + "<td>" + dasanfall_href(member.name) + "</td>"
+        var even_row = (member_counter % 2 === 0) ? 'class="even_row"' : "";
+        membersHTML += "<tr " + even_row + " >"
+            + "<td class=\"right_align\">" + member_counter + "</td>"
+            + "<td>"
+                + '<button name="' + member.name + '" ' + ' class="show_extra">+</button>'
+                + dasanfall_href(member.name)
+            + "</td>"
             + "<td>" + member.rank + "</td>"
             + "<td>" + member.battle_rank + "</td>"
-            + "<td>" + member.member_since_date + "</td>"
+            + "<td>" + strip_hour_from_SOE_date(member.member_since_date) + "</td>"
         // + "<td>" + member.last_save_date + "</td>"
         //add each month, could later make this variable
         for (month_index in member.playtime_per_month) {
@@ -289,13 +393,46 @@ function create_members_HTML(members) {
         membersHTML += "</tr>";
         member_counter += 1;
     }
+    //  membersHTML += "</tbody>";
 
     //end table
     membersHTML += "</table>";
 
-    $("#members").html(membersHTML);
+    return membersHTML;
+}
 
 
+function create_member_extra_HTML(member) {
+    console.log(member);
+    console.log(member.name);
+    var member_HTML = "";
+    member_HTML += '<tr id="' + member.name + '" ><td class="remove_right_border"></td><td class="remove_left_border" colspan="16">';
+    member_HTML += "<h4>"+ member.name + "</h4>";
+    member_HTML += '<table><tr><td>test</td><td>test</td></tr></table>';
+    member_HTML += '</td></tr>';
+    return member_HTML;
+}
+
+function show_member_extra_info() {
+    var par = $(this).parent().parent();  //tr containing the button
+    var member_name = par.children().eq(1).children().eq(1).text();
+    var member = outfit.find_member(member_name);
+    var member_extra_HTML = create_member_extra_HTML(member);
+    par.after(member_extra_HTML);
+    $(this).html("-");
+    $(this).unbind();
+    $(this).bind("click", hide_member_extra_info);
+}
+
+function hide_member_extra_info() {
+    console.log("hide_month");
+
+    //this function is bound to a button with the same name as the id of the tr we need to remove
+    var id_name = '#' + $(this).attr("name");
+    $(id_name).remove();
+    $(this).html("+");
+    $(this).unbind();
+    $(this).bind("click", show_member_extra_info);
 }
 
 function dasanfall_href(name) {
@@ -388,6 +525,13 @@ function create_class(month_play_time) {
     return month_class;
 }
 
+//update counter, put on webpage: inform user about the progress
+function update_get_stat_history_counter(stepsize, members) {
+    var possible_upper_bound = parseInt(member_playtimeinfo_done_counter) + parseInt(stepsize);
+    var upper_bound = possible_upper_bound < members.length ? possible_upper_bound : members.length;
+    $("#members").html("</br></br> Waiting for respons for members [" + member_playtimeinfo_done_counter + " - " + upper_bound + "]")
+    member_playtimeinfo_done_counter += stepsize;
+}
 
 
 function sort_members(members, field) {
@@ -399,4 +543,9 @@ function sort_members(members, field) {
         return 0;
     }
     members.sort(compare_month01);
+}
+
+function strip_hour_from_SOE_date(SOE_date) {
+    return SOE_date.slice(0, -11);
+
 }
